a66eaa19e077fd0b7543fd3b3f9c892ec8303131
strozniak <piotrek.strozniak@gmail.com>
2014-12-07 00:55:19 +0100

    added color of is-typing functionality

--- public/client.js
+++ public/client.js
@@ -7,17 +7,18 @@ $('form').submit(function() {
     return false;
 });
 socket.on('chat message', function(msg) {
-    $('#writing').remove();
+    $('#is-typing').remove();
     $('#messages').append($('<li>').text(msg));
 });
 socket.on('user action', function(msg) {
+    $('#is-typing').remove();
     $('#messages').append($('<li class="user-action">').text(msg));
 });
 socket.on('writing', function(user) {
-    if ($('#writing').size() === 0) {
-        $('#messages').append($('<li id="writing">').text(user + ' is writting'));
+    if ($('#is-typing').size() === 0) {
+        $('#messages').append($('<li id="is-typing">').text(user + ' is typing'));
         setTimeout(function() {
-            $('#writing').remove();
+            $('#is-typing').remove();
         }, 1000);
 
     }
